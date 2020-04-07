a46b14ed52dd69e4edf4f367f966596ec8d8bf4b
Juan Carlos Guerrero <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCdNsutmtQ9nwDfxB1XNjfL6vDU2rZR1leskDHxjaJcQgdZpUfAO2c9vyrJocVGi1dyNMr+K1acWj3qhBquY3e8wEMiryAUB2AKz+NdB7CuNQf+ZQUuSf37BU/QYVPRDMHwBn0YpyOPgQe1U+a5XNw8u4avIZgXpKdrP9XA5K7JEobuPkjtg1/1P2/01mZRcKqN2fp4lw89bmKHkHXm0opZPbwsCUxCLWPmArDs/xjb4RHI81Era2Su55YOmFGJZ6uLFoBfDHqsMK150bPkicazBEbAH4zWZcHfBl0LF9EbrPAEMPmUfjO1tvyfRqaBFsMlunQhUKGXOZ1OY98ZlaAEiA+KZ5/Sb0GkRd47dtZ8nhfDZGWLuXEbUpyI241UOXb4lPNzsPteL5q5QdZXl1bpxELgDHjASoZAFu36feQ4mGNyfEeW6OlqnGY6vQW7vycJbbALl0ZvgULTYPiRAm8Ywsit8Z53+obRCQgqIHDGnzF9DPrFqyw3ticz0ahv9P2lGkrauinQukMwiikkvNt+/rGpUERnqvXVMrYlGL1+Qe9W40wD3v1tP0NTSoPd33bNFOulhCxI7Vh5rcQ4AY5GTlU8UOjsg0rvSX4GqevUu/3XLk7JhkXLLwW4DL0aqfhi6vgCroOEtkGqGlmvSMRGNeM/+mmudw4q/YmOjS357w== juancarlos.guerrero-ext@dominion-global.com>
2020-04-07 06:59:11 +0200

    FIRST COMMIT

---
+++ lib/src/pages/basico_pages.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class BasicoPage extends StatelessWidget {
+  final estiloTitulo = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+  final estiloSubTitulo = TextStyle(fontSize: 18.0, color: Colors.grey);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          _crearImagen(),
+          _crearTitulo(),
+          _crearAcciones(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+        ],
+      ),
+    ));
+  }
+
+  Widget _crearImagen() {
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage(
+            'https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+        height: 200.0,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _crearTitulo() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Embarcadero', style: estiloTitulo),
+                  SizedBox(height: 7.0),
+                  Text('Un lago en Alemania', style: estiloSubTitulo),
+                ],
+              ),
+            ),
+            Icon(Icons.star, color: Colors.red, size: 30.0),
+            Text('41', style: TextStyle(fontSize: 20.0))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearAcciones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _accion(Icons.call, 'CALL'),
+        _accion(Icons.near_me, 'ROUTER'),
+        _accion(Icons.share, 'Share'),
+      ],
+    );
+  }
+
+  Widget _accion(IconData icon, String texto) {
+    return Column(
+      children: <Widget>[
+        Icon(icon, color: Colors.blue, size: 40.0),
+        SizedBox(height: 5.0),
+        Text(texto, style: TextStyle(fontSize: 15.0, color: Colors.blue))
+      ],
+    );
+  }
+
+  Widget _crearTexto() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
+        child: Text(
+          'Et commodo minim do ea officia consectetur occaecat sunt consequat adipisicing. Non ipsum adipisicing irure ut aliqua Lorem do aute deserunt. Minim id laborum ipsum minim duis deserunt deserunt velit. Fugiat et id deserunt culpa quis occaecat. Pariatur irure esse voluptate ut sit et sit commodo eu velit et nostrud in id. Aliquip qui non nostrud pariatur.',
+          textAlign: TextAlign.justify,
+        ),
+      ),
+    );
+  }
+}
